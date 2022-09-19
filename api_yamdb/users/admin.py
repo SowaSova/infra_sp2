0e51ae22664f08fb0205a7694bf130f9b6363c0f
Алексей Николаев <cei4bio@gmail.com>
2022-09-19 19:28:51 +0600

    12th.1st.

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "bio",
+        "first_name",
+        "last_name",
+    )
+    list_editable = ("role",)
+
+
+admin.site.register(User, UserAdmin)
